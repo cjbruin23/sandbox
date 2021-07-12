9b76d45453214a955538dd611a3f10ad5d08ba38
YZ2PJS <YZ2PJS@alsn.onmicrosoft.com>
2021-07-12 10:56:36 -0400

    adding angular material

--- angular-sanbox/src/app/app.module.ts
+++ angular-sanbox/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './features/home/home.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HomeModule],
+  imports: [BrowserModule, AppRoutingModule, HomeModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
