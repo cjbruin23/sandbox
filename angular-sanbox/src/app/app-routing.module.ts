c9e87cc89b638232f87eb79e682435eaf8bd7595
Colin Bruin <YZ2PJS@alsn.com>
2022-05-30 15:07:38 -0600

    not keeping the home componenet permanently on the screen when navigating

--- angular-sanbox/src/app/app-routing.module.ts
+++ angular-sanbox/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './core/home/home.component';
 
 const routes: Routes = [
   {
     path: 'features',
     loadChildren: () =>
       import('./features/features.module').then((m) => m.FeaturesModule),
+  },
+  {
+    path: '',
+    component: HomeComponent,
   },
 ];
 
