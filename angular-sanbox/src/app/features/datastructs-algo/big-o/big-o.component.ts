4af61056d153c0d73344b4a5870be8b7b8b357e3
Colin Bruin <YZ2PJS@alsn.com>
2022-05-30 19:17:23 -0600

    adding a few functions to show space complexity

--- angular-sanbox/src/app/features/datastructs-algo/big-o/big-o.component.ts
+++ angular-sanbox/src/app/features/datastructs-algo/big-o/big-o.component.ts
@@ -9,4 +9,25 @@ export class BigOComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  // This has a space complexity of O(1)
+  // Space complexity does not account for input size
+  // Only defining variable i adds to space complexity, which the let will let go of the value in memory for each iteration
+  spaceComplexity1(n): void {
+    for (let i = 0; i < n.length; i++) {
+      console.log('booooo');
+    }
+  }
+
+  // This has space complexity of 0(n)
+  // hiArray grows with each iteration, adding to memory
+  // The variable i is let go because we ignore constants for BigO
+  // If we didn't it would be O(n+1)
+  spaceComplexity2(n): string[] {
+    let hiArray = [];
+    for (let i = 0; i < n.length; i++) {
+      hiArray[i] = 'hi';
+    }
+    return hiArray;
+  }
 }
