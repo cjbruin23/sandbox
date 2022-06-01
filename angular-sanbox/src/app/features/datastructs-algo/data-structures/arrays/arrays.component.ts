ab10e613c7b6e36a25647145aa4103c6376b94dc
Colin Bruin <YZ2PJS@alsn.com>
2022-06-01 09:42:03 -0600

    cleanup

--- angular-sanbox/src/app/features/datastructs-algo/data-structures/arrays/arrays.component.ts
+++ angular-sanbox/src/app/features/datastructs-algo/data-structures/arrays/arrays.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-arrays',
   templateUrl: './arrays.component.html',
-  styleUrls: ['./arrays.component.scss']
+  styleUrls: ['./arrays.component.scss'],
 })
 export class ArraysComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
