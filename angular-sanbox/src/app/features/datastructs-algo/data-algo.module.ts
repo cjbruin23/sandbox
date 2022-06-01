0e04d9e03ac96451d8674746d59517cb93a0f19b
Colin Bruin <YZ2PJS@alsn.com>
2022-06-01 09:04:05 -0600

    adding array component and service

--- angular-sanbox/src/app/features/datastructs-algo/data-algo.module.ts
+++ angular-sanbox/src/app/features/datastructs-algo/data-algo.module.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BigOComponent } from './big-o/big-o.component';
 import { DataAlgoRoutingModule } from './data-algo-routing.module';
+import { ArraysComponent } from './data-structures/arrays/arrays.component';
 
 @NgModule({
   imports: [CommonModule, DataAlgoRoutingModule],
-  declarations: [BigOComponent],
+  declarations: [BigOComponent, ArraysComponent],
   providers: [],
   exports: [BigOComponent],
 })
